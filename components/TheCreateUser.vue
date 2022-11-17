ae05304821b7dc3b23f879f7a564a867cf23b18d
TonnyMusembi <musembutonnyj@gmail.com>
2022-11-17 11:34:07 +0300

    add

--- components/TheCreateUser.vue
+++ components/TheCreateUser.vue
@@ -36,27 +36,27 @@
       </div>
       <div class="mb-2">
         <label
-          for=""
+          for="Enter-password"
           class="block mb-2 text-sm font-medium text-black-900 dark:text-black-300"
           >Enter password</label
         >
         <input
-          name="new password"
+          name="enter-password"
           type="text"
           class="bg-gray-50 border border-white-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder=" new password"
+          placeholder=" enter-password"
           required
         />
       </div>
       <div class="block mb-3">
-        <label for="" class="block text-sm font-medium text-black-900 mb-2"
+        <label for="confirm-password" class="block text-sm font-medium text-black-900 mb-2"
           >Confirm password</label
         >
         <input
-          name="Confirm new password"
+          name="Confirm-password"
           type="text"
           class="bg-gray-50 border border-white-300 text-white-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="confirm new password "
+          placeholder="confirm-password "
           required
         />
       </div>
