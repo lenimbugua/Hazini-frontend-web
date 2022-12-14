13046bfc8348448bf4d27500b04e655c32c57ed4
TonnyMusembi <musembutonnyj@gmail.com>
2022-12-14 16:56:15 +0300

    searchbar

--- components/TheUsers.vue
+++ components/TheUsers.vue
@@ -1,4 +1,4 @@
-<script setup >
+<script setup>
 import { useAuthStore } from "../stores/login";
 
 const { user } = storeToRefs(useAuthStore());
@@ -18,10 +18,52 @@ const {
   },
 });
 console.log(users.value);
-
 refresh();
 </script>
 <template>
+  <div class="flex justify-end">
+    <form>
+      <label
+        for="default-search"
+        class="mb-2 text-sm font-medium text-gray-500 sr-only dark:text-white"
+        >Search</label
+      >
+      <div class="relative">
+        <div
+          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          class="block w-full p-4 pl-10 text-sm text-gray-400 border border-cyan-300 rounded-lg bg-gray-50 focus:ring-cyan-500 focus:border-cyan-500 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-500 dark:focus:border-cyan-500"
+          placeholder="Search PhoneNumber..."
+          required
+        />
+        <button
+          type="submit"
+          class="text-white absolute right-2.5 bottom-2.5 bg-cyan-600 hover:bg-cyan-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-400"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  </div>
   <Spin v-if="pending" class="h-20 w-20 text-teal-700" />
   <div
     v-else
