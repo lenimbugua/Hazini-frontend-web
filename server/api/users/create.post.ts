54b8f6fa7014f2ca29ba83a0487be2415a3833b6
lenimbugua <lenimbugua@gmail.com>
2022-12-15 18:59:57 +0300

    added created_at field in users

--- server/api/users/create.post.ts
+++ server/api/users/create.post.ts
@@ -1,7 +1,7 @@
 const runtimeConfig = useRuntimeConfig();
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-  const createdUser = await $fetch("/users/bulk", {
+  const createdUser = await $fetch("/users", {
     body: body,
     method: "POST",
     baseURL: runtimeConfig.baseURL,
